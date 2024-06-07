b31db8972b333a515e458417cdada98596d611d3
Original-pokemon <eremkin-02@mail.ru>
2024-06-07 18:10:49 +0300

    refactor: delete unused var

--- src/manager/ManagerList.tsx
+++ src/manager/ManagerList.tsx
@@ -1,23 +1,38 @@
 import {
+  ArrayField,
+  Count,
   Datagrid,
+  Error,
   List,
+  Loading,
+  ReferenceArrayField,
   ReferenceField,
   ReferenceInput,
+  ReferenceManyCount,
+  ReferenceManyField,
+  ReferenceOneField,
   TextField,
+  WithListContext,
   WrapperField,
+  useGetOne,
 } from "react-admin";
 import { TicketCountField } from "./TicketCountField";
 import { PetrolStationCountField } from "./PetrolStationCountField";
+import { Chip, Stack } from '@mui/material';
+import { ManagerType } from "../types";
 
 export const ManagerList = () => (
   <List
     title="Менеджеры"
-    filters={[<ReferenceInput source="bush_id" label="Куст" reference='bush' alwaysOn />]}
+    filters={[
+      <ReferenceInput source="bush_id" label="Куст" reference='bush' alwaysOn />
+    ]}
     perPage={25}
   >
     <Datagrid rowClick="show" bulkActionButtons={false}>
       <TextField source="user.user_name" resource="manager" label="Менеджер" />
       <ReferenceField resource="description" source="bush_id" reference="bush" label="Куст" />
+
       <WrapperField label="Задачи">
         <TicketCountField />
       </WrapperField>
