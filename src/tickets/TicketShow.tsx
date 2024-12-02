01c0cd18770142db1c1b5cea3e9ed5bb8b975b1f
original-pokemon <eremkin-02@mail.ru>
2024-12-02 15:54:09 +0300

    feat: show user in status history for ticket show

--- src/tickets/TicketShow.tsx
+++ src/tickets/TicketShow.tsx
@@ -11,24 +11,39 @@ import {
   SingleFieldList,
   ArrayField,
   ChipField,
+  useStore,
+  FunctionField,
 } from "react-admin";
-import { Box } from "@mui/material";
+import { Chip } from "@mui/material";
 import { CommentList } from "./CommentList";
 import AttachmentImageField from "./fields/AttachmentImageField";
+import { StoreKey } from "../preload-data/preload-data";
+import getDescription from "../utils/get-desctiption";
+import type { CategoryType, PetrolStationType, TicketType } from "../types";
 
 export const TicketShow = () => {
+  const [categoriesData] = useStore<CategoryType[]>(StoreKey.CATEGORY_DATA, []);
+  const [petrolStationsData] = useStore<PetrolStationType[]>(StoreKey.PETROL_STATION_DATA, []);
+
   return (
     <Show aside={<ShowAside />} queryOptions={{ refetchOnMount: "always" }}>
       <SimpleShowLayout spacing={2} >
         <TextField source="title" label={false} variant="h5" />
         <TextField source="description" label="Описание" variant="body1" />
-        <ReferenceField source="petrol_station_id" reference="petrol-station" label="АЗС" >
-          <ChipField source="user.user_name" />
-        </ReferenceField>
-        <ReferenceField source="ticket_category" reference="category" label="Категория">
-          <ChipField source="description" />
-        </ReferenceField>
 
+        <FunctionField
+          label="АЗС"
+          render={(record: TicketType) => (
+            <Chip label={petrolStationsData.find((entry) => entry.id === record.petrol_station_id)?.user?.user_name} />
+          )}
+        />
+
+        <FunctionField
+          label="Категория"
+          render={(record: TicketType) => (
+            <Chip label={getDescription(record.ticket_category, categoriesData)} />
+          )}
+        />
         <DateField source="deadline" label="Заявленная дата исполнения" />
 
         <ReferenceArrayField source='attachments' reference="attachment" sort={{ field: "created_at", order: "ASC" }} label="Вложения">
@@ -52,15 +67,26 @@ export const TicketShow = () => {
 };
 
 const ShowAside = () => {
-  const record = useRecordContext();
-  if (!record) return <Box minWidth={200} flexShrink={0} />;
+  const ticketRecord = useRecordContext<TicketType>();
+  const [statusesData] = useStore<CategoryType[]>(StoreKey.STATUS_DATA, []);
+  if (!ticketRecord || !statusesData.length) return null;
+
   return (
     <SimpleShowLayout sx={{ minWidth: 200, flexShrink: 0 }}>
       <ArrayField label="История изменений" source="status_history" sortBy="created_at">
         <Datagrid bulkActionButtons={false}>
-          <ReferenceField reference="status" source="ticket_status" label="Статус" >
-            <ChipField source="description" />
-          </ReferenceField>
+
+          <FunctionField
+            label="Статуc"
+            render={(record: { ticket_status: string }) => {
+              return (
+                <Chip label={getDescription(record.ticket_status, statusesData)} />
+              )
+            }}
+          />
+
+          <ReferenceField label="Пользователь" reference="user" source="user_id" />
+
           <DateField source="created_at" label="Дата" showTime locales={"ru"} />
         </Datagrid>
       </ArrayField>
